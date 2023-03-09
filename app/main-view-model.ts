d9d8b43ac631c84af0bf3980609b068a6fa83d6a
Todd Cermak <tcermak@goempyrean.com>
2023-03-09 10:36:23 -0600

    Test for iOS share sheets.

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,4 +1,5 @@
-import { Observable } from '@nativescript/core'
+import { Application, ImageSource, Observable, Frame, Utils } from '@nativescript/core'
+import { ios } from '@nativescript/core/application'
 
 export class HelloWorldModel extends Observable {
   private _counter: number
@@ -10,7 +11,30 @@ export class HelloWorldModel extends Observable {
     // Initialize default values.
     this._counter = 42
     this.updateMessage()
+
+    // Let's share something in iOS.
+    const activityController = UIActivityViewController.alloc()
+      .initWithActivityItemsApplicationActivities(["Text to share"], null);
+
+    const presentViewController = activityController.popoverPresentationController;
+    if (presentViewController) {
+      const page = Frame.topmost().currentPage;
+      if (page && page.ios.navigationItem.rightBarButtonItems &&
+      page.ios.navigationItem.rightBarButtonItems.count > 0) {
+      presentViewController.barButtonItem = page.ios.navigationItem.rightBarButtonItems[0];
+    } else {
+      presentViewController.sourceView = page.ios.view;
+    }
   }
+
+  const app = UIApplication.sharedApplication;
+  const window = app.keyWindow || (app.windows && app.windows.count > 0 && app.windows[0]);
+  const rootController = window.rootViewController;
+
+  Utils.ios.getVisibleViewController(rootController)
+    .presentViewControllerAnimatedCompletion(activityController, true, null);
+  }
+  
 
   get message(): string {
     return this._message
@@ -26,6 +50,7 @@ export class HelloWorldModel extends Observable {
   onTap() {
     this._counter--
     this.updateMessage()
+    
   }
 
   private updateMessage() {
